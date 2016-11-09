d8c8295b14569c664750f8165d9665e7ed564f18
ly1314529 <1056510359@qq.com>
2016-11-09 15:27:45 +0800

    Update when.js

--- test/steps/when.js
+++ test/steps/when.js
@@ -1,1 +1,7 @@
 
+module.exports=function() {
+	this
+	    .when(/^I (\s+)(click|doubleclick) (\s+)on (\s+)the (\s+)(link|element|button) (\s+)"$string"$/,
+		     require('../support/action/clickElement'))
+
+};
