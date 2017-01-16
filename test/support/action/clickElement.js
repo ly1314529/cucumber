cac2e5394dac9aef53c40298124e696aa5b41202
ly1314529 <1056510359@qq.com>
2017-01-16 13:45:19 +0800

    Update clickElement.js

--- test/support/action/clickElement.js
+++ test/support/action/clickElement.js
@@ -6,3 +6,12 @@ module.exports=function (type,action,element,done) {
 	     .pause(5000)
 	     .call(done);
 };
+
+
+
+/*
+参数（执行的动作，类型，被执行的元素，回调）
+判定被执行的元素类型，定义为element
+判定执行动作的方法，单击，双击
+执行动作，任务完成后，回调
+*/
